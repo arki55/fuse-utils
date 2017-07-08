925833a699cca78b74e409205545f111b802dcd1
Fredrick Meunier <fredm@spamcop.net>
2017-07-08 19:54:45 +1000

    Extract compressed block reading routines

--- fmfconv.c
+++ fmfconv.c
@@ -55,6 +55,7 @@
 
 #include "compat.h"
 #include "fmfconv.h"
+#include "fmfconv_compr.h"
 #include "movie_tables.h"
 
 #define PROGRAM_NAME "fmfconv"
@@ -63,15 +64,6 @@
 #define VERBOSE_MIN -1
 
 const char *progname;
-
-#ifdef USE_ZLIB
-#define ZBUF_SIZE 1024
-#define ZBUF_INP_SIZE 512
-z_stream zstream;			/* zlib struct */
-unsigned char zbuf_o[ZBUF_SIZE];	/* zlib output buffer */
-unsigned char zbuf_i[ZBUF_INP_SIZE];	/* zlib input buffer */
-int fmf_compr_feof = 0;
-#endif	/* USE_ZLIB */
 
 #ifdef USE_LIBJPEG
 extern int jpg_dctfloat;
@@ -207,7 +199,6 @@ int out_header_ok = 0;			/* output header ok? */
 int out_chn = 2, out_rte = -1, out_fsz, out_len;	/* by default convert sound to 2 channel (STEREO) */
 
 /* fmf variables */
-int fmf_compr = 0;			/* fmf compressed or not */
 int fmf_little_endian = 0;		/* little endian PCM */
 int snd_little_endian = -1;			/* little endian PCM */
 libspectrum_qword fmf_slice_no = 0;
@@ -293,77 +284,6 @@ fread_buff( libspectrum_byte *ptr, size_t size, int how )	/* read from inp, how 
   }
   return 1;
 }
-
-#ifdef USE_ZLIB
-static int
-feof_compr( FILE *f )
-{
-  if( fmf_compr ) {
-    if( fmf_compr_feof )
-      return 1;
-    if( zstream.avail_in > 0 || zstream.avail_out != ZBUF_SIZE )
-      return 0;
-    fmf_compr_feof = feof( f );
-    return fmf_compr_feof;
-  } else {
-    return feof( f );
-  }
-}
-
-int fget_read = 0;
-
-static int
-fgetc_compr( FILE *f )
-{
-  int s;
-
-  if( fmf_compr ) {
-    if( zstream.avail_out == ZBUF_SIZE ) {
-      fget_read++;
-      zstream.next_out = zbuf_o;
-      do {
-	if( zstream.avail_in == 0 && !fmf_compr_feof ) {
-	  zstream.avail_in = fread( zbuf_i, 1, ZBUF_INP_SIZE, inp_file );
-	  zstream.next_in = zbuf_i;
-	  if( zstream.avail_in == 0 )
-	    fmf_compr_feof = 1;
-	}
-	s = inflate( &zstream, fmf_compr_feof ? Z_FINISH : Z_SYNC_FLUSH );
-      } while ( zstream.avail_out != 0 && s == Z_OK );
-      zstream.next_out = zbuf_o;
-    }
-    if( zstream.avail_out == ZBUF_SIZE )	/* end of file */
-      return -1;
-    zstream.avail_out++;
-    return *( zstream.next_out++ );    /* !!! we use it for own purpose */
-  } else {
-    return fgetc( f );
-  }
-}
-
-static int
-fread_compr( void *buff, size_t n, size_t m, FILE *f )
-{
-  size_t i;
-  int d;
-  char *b = buff;
-
-  if( fmf_compr ) {
-    for( i = n * m; i > 0; i-- ) {
-      if( ( d = fgetc_compr( f ) ) == -1 )	/****FIXME may too slow */
-        return ( n * m - i ) / n;
-      *b++ = d;
-    }
-    return m;
-  } else {
-    return fread( b, n, m, f );
-  }
-}
-#else	/* USE_ZLIB */
-#define fgetc_compr fgetc
-#define fread_compr fread
-#define feof_compr feof
-#endif	/* USE_ZLIB */
 
 libspectrum_dword
 swap_endian_dword( libspectrum_dword d )
@@ -2183,13 +2103,7 @@ main( int argc, char *argv[] )
   else if( sound_stereo == 0 ) out_chn = 1;
 
 #ifdef USE_ZLIB
-  zstream.zalloc = Z_NULL;
-  zstream.zfree = Z_NULL;
-  zstream.opaque = Z_NULL;
-  zstream.avail_in = 0;
-  zstream.next_in = Z_NULL;
-  inflateInit( &zstream );
-  zstream.avail_out = ZBUF_SIZE;
+  fmf_compr_init();
 #endif	/* USE_ZLIB */
 
 #ifdef HAVE_SIGNAL
@@ -2266,7 +2180,7 @@ main( int argc, char *argv[] )
   }
 
 #ifdef USE_ZLIB
-  inflateEnd( &zstream );
+  fmf_compr_end();
 #endif	/* USE_ZLIB */
 
   if( ( out_t >= TYPE_SCR && out_t <= TYPE_JPEG ) && out_name ) unlink( out_name );
