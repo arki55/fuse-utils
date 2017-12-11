02108c022ffe545ba43c3a09a6d18a3f34e0aa0f
Sergio Baldoví <serbalgi@gmail.com>
2017-12-11 23:43:51 +0100

    Don't print UTF-8 characters on stdout

--- snapdump.c
+++ snapdump.c
@@ -774,7 +774,7 @@ show_version( void )
 {
   printf(
    PROGRAM_NAME " (" PACKAGE ") " PACKAGE_VERSION "\n"
-   "Copyright (c) 2017 Sergio Baldoví\n"
+   "Copyright (c) 2017 Sergio Baldovi\n"
    "License GPLv2+: GNU GPL version 2 or later "
    "<http://gnu.org/licenses/gpl.html>\n"
    "This is free software: you are free to change and redistribute it.\n"
